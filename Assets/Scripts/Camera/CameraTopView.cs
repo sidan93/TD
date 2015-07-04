bebdc5d36f17645f7602204f0763d72c7ca73b33
Сидоров А.Б <sidan93@gmail.com>
2015-07-04 12:58:18 +0600

    Добавил класс для управление камерой

---
+++ Assets/Scripts/Camera/CameraTopView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTopView : MonoBehaviour {
+
+    // Переменные для установки из интерфейса
+    public Terrain terrain;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(Input.mousePosition.ToString());
+	}
+}
